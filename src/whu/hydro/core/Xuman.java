dd5c17303e5844e832587d47dc352e92b0ee3a0b
gavin <while@whu.edu.cn>
2019-02-20 21:43:23 +0800

    add hydro model of tank

---
+++ src/whu/hydro/core/Xuman.java
@@ -0,0 +1,11 @@
+package whu.hydro.core;
+
+/**
+ * @ClassName Xuman
+ * @Description TODO
+ * @Author 86187
+ * @Date 2019/2/20 13:44
+ * @Version 1.0
+ */
+public class Xuman {
+}
---
+++ src/whu/hydro/core/model/Tank.java
@@ -0,0 +1,12 @@
+package whu.hydro.core.model;
+
+/**
+ * @ClassName Tank
+ * @Description TODO
+ * @Author 86187
+ * @Date 2019/2/20 21:38
+ * @Version 1.0
+ */
+public class Tank {
+
+}
